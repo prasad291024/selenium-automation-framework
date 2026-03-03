530b17799f48be007269590bbbab0d82d5c94e94
Prasad <prasadvaliv92@gmail.com>
2026-03-03 16:55:58 +0530

    feat: complete phase 4 - integrate ConfigManager in all tests

--- src/test/java/com/prasad_v/tests/pageFactory/TestVWOLogin_PF.java
+++ src/test/java/com/prasad_v/tests/pageFactory/TestVWOLogin_PF.java
@@ -2,8 +2,7 @@ package com.prasad_v.tests.pageFactory;
 
 import com.prasad_v.base.CommonToAllTest;
 import com.prasad_v.pages.pageFactory.appvwo.LoginPage_PF;
-import com.prasad_v.utils.PropertiesReader;
-import org.openqa.selenium.WebDriver;
+import com.prasad_v.utils.ConfigManager;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
@@ -12,17 +11,14 @@ import org.apache.logging.log4j.Logger;
 
 import static com.prasad_v.driver.DriverManager.getDriver;
 
-
 public class TestVWOLogin_PF extends CommonToAllTest {
 
-
     private static final Logger logger = LogManager.getLogger(TestVWOLogin_PF.class);
-
 
     @Test
     public void testLoginNegativeVWO_PF() {
         logger.info("Starting the Testcases Page Factory");
-        getDriver().get(PropertiesReader.readKey("url"));
+        getDriver().get(ConfigManager.get("url"));
         LoginPage_PF loginPage_PF = new LoginPage_PF(getDriver());
         String error_msg = loginPage_PF.loginToVWOInvalidCreds();
         logger.info("Error msg I got "+ error_msg);
@@ -30,6 +26,6 @@ public class TestVWOLogin_PF extends CommonToAllTest {
         logger.debug("Finished the Testcases Page Factory");
         logger.error("Finished the Testcases Page Factory");
         logger.fatal("Fatal error - code is dead!");
-        Assert.assertEquals(error_msg, PropertiesReader.readKey("error_message"));
+        Assert.assertEquals(error_msg, ConfigManager.get("error_message"));
     }
 }
--- src/test/java/com/prasad_v/tests/pom/vwo/ddt/test_DDT_TestNG_POI_REAL_VWOLOGIN.java
+++ src/test/java/com/prasad_v/tests/pom/vwo/ddt/test_DDT_TestNG_POI_REAL_VWOLOGIN.java
@@ -2,48 +2,29 @@ package com.prasad_v.tests.pom.vwo.ddt;
 import com.prasad_v.UtilsExcel.UtilExcel;
 import com.prasad_v.base.CommonToAllTest;
 import com.prasad_v.pages.pageObjectModel.appvwo.imporved_POM.LoginPage;
-import com.prasad_v.utils.PropertiesReader;
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.edge.EdgeDriver;
-import org.openqa.selenium.edge.EdgeOptions;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
+import com.prasad_v.utils.ConfigManager;
 import org.testng.Assert;
-import org.testng.annotations.AfterTest;
-import org.testng.annotations.BeforeTest;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
-import java.time.Duration;
-
 import static com.prasad_v.driver.DriverManager.getDriver;
-
 
 public class test_DDT_TestNG_POI_REAL_VWOLOGIN extends CommonToAllTest {
 
-
-
     @Test(dataProvider = "getData")
-    public void test_vwo_login(String email,String password) {
+    public void test_vwo_login(String email, String password) {
         System.out.println(email + " - "+ password);
-        getDriver().navigate().to(PropertiesReader.readKey("url"));
-        Assert.assertEquals(getDriver().getTitle(), PropertiesReader.readKey("current_title"));
-        Assert.assertEquals(getDriver().getCurrentUrl(), PropertiesReader.readKey("current_url"));
+        getDriver().navigate().to(ConfigManager.get("url"));
+        Assert.assertEquals(getDriver().getTitle(), ConfigManager.get("current_title"));
+        Assert.assertEquals(getDriver().getCurrentUrl(), ConfigManager.get("current_url"));
 
         LoginPage loginPage = new LoginPage(getDriver());
-        String error_message =  loginPage.loginToVWOLoginInvalidCreds(email,password);
-        Assert.assertEquals(error_message, PropertiesReader.readKey("error_message"));
-
+        String error_message = loginPage.loginToVWOLoginInvalidCreds(email, password);
+        Assert.assertEquals(error_message, ConfigManager.get("error_message"));
     }
-
 
     @DataProvider(parallel = true)
     public Object[][] getData(){
-        // READ THE DATA FROM THE EXCEL FILE
-        // GIVE THEM IN THE 2D ARRAY
         return UtilExcel.getTestDataFromExcel("sheet1");
-
     }
-
 }
