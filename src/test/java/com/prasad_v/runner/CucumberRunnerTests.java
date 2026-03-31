e416399f8d2f2681a446a6c81ce14ee69a3ce765
Prasad V <prasadvaliv92@gmail.com>
2026-03-31 21:36:56 +0530

    feat: cucumber BDD integration with POM, DriverManagerTL, Allure and parallel execution (#22)

--- src/test/java/com/prasad_v/runner/CucumberRunnerTests.java
+++ src/test/java/com/prasad_v/runner/CucumberRunnerTests.java
@@ -2,14 +2,25 @@ package com.prasad_v.runner;
 
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
+import org.testng.annotations.DataProvider;
 
-
-@CucumberOptions(tags = "",
-        features = {"src/test/resources/features/VWOLogin.feature"},
-        glue = {"com.prasad_v.definitions"},
-        plugin = { "pretty", "html:target/cucumber-reports.html"}
+@CucumberOptions(
+        features = "src/test/resources/features",
+        glue = "com.prasad_v.definitions",
+        tags = "not @Ignore",
+        plugin = {
+                "pretty",
+                "html:target/cucumber-reports/cucumber.html",
+                "json:target/cucumber-reports/cucumber.json",
+                "io.qameta.allure.cucumber7jvm.AllureCucumber7Jvm"
+        },
+        monochrome = true
 )
-
 public class CucumberRunnerTests extends AbstractTestNGCucumberTests {
 
+    @Override
+    @DataProvider(parallel = true)
+    public Object[][] scenarios() {
+        return super.scenarios();
+    }
 }
