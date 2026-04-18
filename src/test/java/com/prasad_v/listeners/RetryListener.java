61c2cb50216ba5a341b4444ec4bea958d319d1f2
Prasad <prasadvaliv92@gmail.com>
2026-04-18 19:02:53 +0530

    Implement global TestNG retry mechanism across suites and CI

--- src/test/java/com/prasad_v/listeners/RetryListener.java
+++ src/test/java/com/prasad_v/listeners/RetryListener.java
@@ -9,6 +9,8 @@ import java.lang.reflect.Method;
 public class RetryListener implements IAnnotationTransformer {
     @Override
     public void transform(ITestAnnotation annotation, Class testClass, Constructor testConstructor, Method testMethod) {
-        annotation.setRetryAnalyzer(RetryAnalyzer.class);
+        if (annotation.getRetryAnalyzerClass() == null) {
+            annotation.setRetryAnalyzer(RetryAnalyzer.class);
+        }
     }
 }
