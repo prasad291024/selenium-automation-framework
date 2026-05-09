97ee094a722de3a15a142d662dac63c82f464985
Prasad <prasadvaliv92@gmail.com>
2026-05-09 18:21:01 +0530

    feat: wire retry and screenshot listeners across all apps

--- src/test/java/com/prasad_v/base/CommonToAllTest.java
+++ src/test/java/com/prasad_v/base/CommonToAllTest.java
@@ -1,18 +1,24 @@
 package com.prasad_v.base;
 
 import com.prasad_v.driver.DriverManagerTL;
+import com.prasad_v.listeners.ScreenshotListener;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Listeners;
 
+@Listeners({ ScreenshotListener.class })
 public class CommonToAllTest {
 
+    protected org.openqa.selenium.WebDriver driver;
+
     @BeforeMethod
-    public void setUp(){
+    public void setUp() {
         DriverManagerTL.init();
+        driver = DriverManagerTL.getDriver();
     }
 
     @AfterMethod
-    public void tearDown(){
+    public void tearDown() {
         DriverManagerTL.quit();
     }
 }
