be298ccca1895a8321c5ca48964f0036a9db9c0c
Prasad <prasadvaliv92@gmail.com>
2026-05-10 22:53:28 +0530

    feat: add data-driven tests for all 3 apps with per-app Excel test data

---
+++ src/test/java/com/prasad_v/apps/katalon/tests/KatalonLoginDDTTest.java
@@ -0,0 +1,44 @@
+package com.prasad_v.apps.katalon.tests;
+
+import com.prasad_v.apps.katalon.pages.HomePage;
+import com.prasad_v.apps.katalon.pages.LoginPage;
+import com.prasad_v.base.CommonToAllTest;
+import com.prasad_v.driver.DriverManagerTL;
+import com.prasad_v.utils.ExcelUtil;
+import com.prasad_v.utils.LoggerUtil;
+import io.qameta.allure.*;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@Epic("Katalon Application")
+@Feature("Login - Data Driven")
+public class KatalonLoginDDTTest extends CommonToAllTest {
+
+    private static final String DATA_FILE = "src/test/resources/testdata/KatalonTestData.xlsx";
+
+    @DataProvider(name = "invalidLoginData", parallel = true)
+    public Object[][] invalidLoginData() {
+        return ExcelUtil.getTestData(DATA_FILE, "InvalidLogin");
+    }
+
+    @Test(dataProvider = "invalidLoginData")
+    @Story("Invalid Login - Data Driven")
+    @Severity(SeverityLevel.NORMAL)
+    @Description("Verify error message for multiple invalid credential combinations from Excel")
+    public void testInvalidLoginDDT(String username, String password, String expectedError) {
+        LoggerUtil.info("DDT - Invalid login test | user: " + username);
+        HomePage homePage = new HomePage(DriverManagerTL.getDriver());
+        LoginPage loginPage = new LoginPage(DriverManagerTL.getDriver());
+
+        homePage.goToLogin();
+        String actualError = loginPage.loginWithInvalidCreds(username, password);
+
+        assertThat(actualError)
+                .as("Error message should match for user: " + username)
+                .contains(expectedError);
+
+        Allure.addAttachment("Test Data", "User: " + username + " | Expected: " + expectedError);
+    }
+}
